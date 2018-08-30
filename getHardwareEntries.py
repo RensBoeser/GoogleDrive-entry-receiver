ae0100b6b9cb1a13d3a846c3d0782f34e013bd1e
Tom van Dijk <tevd99@hotmail.com>
2018-08-30 12:25:45 +0200

    Function returns JSON now

--- getHardwareEntries.py
+++ getHardwareEntries.py
@@ -1,10 +1,8 @@
 from driveAPIHandler import DriveService
-import os
+import os, csv, json
 
-def GetEntries():
+def GetHardwareEntries(libpath):
 	# Designate entries file
-	dirname  = os.path.dirname
-	libpath = dirname(os.path.realpath(__file__)) + '/lib/'
 	print(libpath)
 	fileId = '1AVZwyLwWoGhELpZ5hwOA3FQ_NxxKq40I4csJ8F4neYs'
 	fileType = 'text/csv'
@@ -12,8 +10,20 @@ def GetEntries():
 
 	# Download file
 	service = DriveService(libpath)
-	service.GetFile(fileId, fileType, fileDestination)
+	service.GetFile(fileId, fileType, '../' + fileDestination)
+	result = "{\"entries\":[\n"
+	with open(fileDestination, 'r') as csvfile:
+		reader = csv.DictReader(csvfile)
+		for row in reader:
+			result += json.dumps(row)
+			result += ",\n"
+		result = result[0:len(result)-2] + "\n]}"
+
+	os.remove(fileDestination)
+	return result
 
 if __name__ == '__main__': #Debug code
 	print('Start debug code')
-	GetEntries()
+	dirname  = os.path.dirname
+	libpath = dirname(os.path.realpath(__file__)) + '/lib/'
+	print(GetHardwareEntries(libpath))
